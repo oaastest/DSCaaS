f6fa17ed34010e0d0bc8d6dededa49952b6deb31
oaastest <oaastest@live.com>
2015-07-15 12:36:32 -0700

    Created NewRunbook.ps1 in AzureAutomation.

---
+++ vs2013/NewRunbook.ps1
@@ -0,0 +1,6 @@
+workflow NewRunbook {
+	Write-Output "This change was made in Azure Automation and committed to the right directory."
+	Get-Date
+	Get-Date
+	Get-Date #This change was made in Azure Automation
+}
